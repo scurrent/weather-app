31db9902ee7003c2c23007586455e3b92d1e2769
Scott-Current <Scott.Current@officedepot.com>
2019-06-29 10:24:50 -0400

    data update

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,23 +8,15 @@ const forecast = (lat, long, callback) => {
     const url = "https://api.darksky.net/forecast/7a833d287462f592917f1b3a7173f061/" + lat +","+ long
     //url: url   -> destuct to just url
     //response destructures to just body
-    request({url, json: true},  (error, {body})=> {
-            console.log(url)
-   
-        if(error){
-            callback("unable to connect to weather service", undefined)  
-        }else if(body.error){
-            callback("unable to find the location", undefined)
-        
-        }else{
-            const result = body.daily.data[0].summary + " It is currently " + body.currently.temperature + " degrees.  There is a " + body.currently.precipProbability + "% chance of rain."
-            console.log(result)
-            callback(undefined, result)
-        }    
-//        console.log("It is currently " + response.body.currently.temperature + " degrees.  There is a " + response.body.currently.precipProbability + "% chance of rain." )
- //       console.log(response.body.daily.data[0].summary)
-    
-    })    
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. This high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        }
+    })   
 }
 
 
